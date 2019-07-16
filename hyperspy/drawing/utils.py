e244da7af99feaad86ecfbba3b654a4be5f54c2c
AndrewHerzing <aaherzing@gmail.com>
2019-07-16 13:31:10 -0400

    Added merge_color_channels

--- hyperspy/drawing/utils.py
+++ hyperspy/drawing/utils.py
@@ -233,6 +233,140 @@ def subplot_parameters(fig):
     return left, bottom, right, top, wspace, hspace
 
 
+def merge_color_channels(im_list, color_list=None,
+                         normalization='single', return_all_channels=False):
+    """
+    Merge up to six gray scale images into a color composite.
+    Parameters
+    ----------
+    im_list : list of Signal2D instances (images) to be merged
+    color_list : list of strings
+        Should be valid matplotlib color strings, such as 'red', 'green',
+        'blue, etc.
+    normalization : str
+        Either 'single' or 'global'; controls whether the color scales are
+        normalized on a per-image basis (``'single'``), or globally over all
+        images (``'global'``)
+    return_all_channels : bool
+        If False, return just the RGB overlay signal.  If True, return a
+        a dictionary with the RGB overlay and each individual color channel.
+        Default is False.
+    Returns
+    -------
+    array: Signal1D or Dictionary
+        A Signal1D containing the RGB overlay (return_all_channels is False,
+        default behavior) or a dictionary with the RGB overlay and each
+        individual color channel (if return_all_channels is True)
+    """
+    # TODO: Can itertools.cycle be used here to remove the 6 image limit?
+    color_cycle = ['red', 'green', 'blue', 'cyan', 'yellow', 'magenta', 'gray']
+    if len(im_list) > 7:
+        raise ValueError('List must be at most 7 images long')
+    if color_list is None:
+        color_list = color_cycle[0:len(im_list)]
+    if not all(x in color_cycle for x in color_list):
+        raise ValueError("Invalid color. Only red, green, blue, cyan, yellow "
+                         "magenta and gray allowed")
+
+    # shapes is (N, 2) numpy array containing the height and width of each image
+    shapes = np.asarray([im.data.shape[:2] for im in im_list])
+    # Compare all rows of shapes to first row, and check if all rows in each
+    # column are identical
+    isequal = np.all(shapes == shapes[0, :], axis=0)
+    # isequal should be [True, True], if it's not, raise an error:
+    if not np.all(isequal):
+        raise ValueError("All images must be the same shape to build "
+                         "composite. Image shapes were: \n{}".format(shapes))
+    height, width = shapes[0, :]
+
+    images = dict()
+    images['rgb'] = np.zeros([height, width, 3])
+    for i in color_list:
+        images[i] = np.zeros([height, width, 3])
+
+    def _normalize_channels(im_list, color_list, method, iter_number):
+        # determine normalization denominator
+        if method == 'single':
+            norm_value = im_list[iter_number].data.max()
+        elif method == 'global':
+            maxvals = np.zeros(len(im_list))
+            for im_num in range(0, len(im_list)):
+                maxvals[im_num] = im_list[im_num].data.max()
+            maxval = maxvals.max()
+            norm_value = maxval
+        else:
+            raise ValueError("Unknown normalization method."
+                             "Must be 'single' or 'global'.")
+
+        if color_list[iter_number] == 'red':
+            images['red'][:, :, 0] = \
+                im_list[iter_number].data / norm_value  # red channel
+        elif color_list[iter_number] == 'green':
+            images['green'][:, :, 1] = \
+                im_list[iter_number].data / norm_value  # green channel
+        elif color_list[iter_number] == 'blue':
+            images['blue'][:, :, 2] = \
+                im_list[iter_number].data / norm_value  # blue channel
+        elif color_list[iter_number] == 'yellow':
+            images['yellow'][:, :, 0] = \
+                im_list[iter_number].data / norm_value  # red channel +
+            images['yellow'][:, :, 1] = \
+                im_list[iter_number].data / norm_value  # green channel
+        elif color_list[iter_number] == 'magenta':
+            images['magenta'][:, :, 0] = \
+                im_list[iter_number].data / norm_value  # red channel +
+            images['magenta'][:, :, 2] = \
+                im_list[iter_number].data / norm_value  # blue channel
+        elif color_list[iter_number] == 'cyan':
+            images['cyan'][:, :, 1] = \
+                im_list[iter_number].data / norm_value  # green channel +
+            images['cyan'][:, :, 2] = \
+                im_list[iter_number].data / norm_value  # blue channel
+        elif color_list[iter_number] == 'gray':
+            images['gray'][:, :, 0] = \
+                im_list[iter_number].data / norm_value / 2  # red channel +
+            images['gray'][:, :, 1] = \
+                im_list[iter_number].data / norm_value / 2  # green channel +
+            images['gray'][:, :, 2] = \
+                im_list[iter_number].data / norm_value / 2  # blue channel
+        else:
+            raise ValueError("Unknown color. Must be red, green, blue, "
+                             "yellow, magenta, cyan, or gray.")
+
+    for i in range(0, len(im_list)):
+        # DONE: move normalization into function
+        _normalize_channels(im_list, color_list, normalization, i)
+
+    # Scale RGB images between 0 and 255 and convert to int32 to avoid
+    # matplotlib warning
+    for i in color_list:
+        images['rgb'] += images[i]
+
+    images['rgb'] = np.uint8(images['rgb'] * (255 / images['rgb'].max()))
+    images['rgb'] = hs.signals.Signal1D(images['rgb'])
+    images['rgb'].change_dtype("rgb8")
+    for i in range(0, 2):
+        images['rgb'].axes_manager[i].name = im_list[0].axes_manager[i].name
+        images['rgb'].axes_manager[i].offset = im_list[0].axes_manager[i].offset
+        images['rgb'].axes_manager[i].scale = im_list[0].axes_manager[i].scale
+        images['rgb'].axes_manager[i].units = im_list[0].axes_manager[i].units
+
+    if return_all_channels:
+        for i in color_list:
+            images[i] = np.uint8(images[i] * (255 / images[i].max()))
+            images[i] = hs.signals.Signal1D(images[i])
+            images[i].change_dtype("rgb8")
+
+            for j in range(0, 2):
+                images[i].axes_manager[j].name = im_list[0].axes_manager[j].name
+                images[i].axes_manager[j].offset = im_list[0].axes_manager[j].offset
+                images[i].axes_manager[j].scale = im_list[0].axes_manager[j].scale
+                images[i].axes_manager[j].units = im_list[0].axes_manager[j].units
+        return images
+    else:
+        return images['rgb']
+
+
 class ColorCycle:
     _color_cycle = [mpl.colors.colorConverter.to_rgba(color) for color
                     in ('b', 'g', 'r', 'c', 'm', 'y', 'k')]
